26bfa857d2324f997dcf751acfbde5b44a40e8ee
Amogh Raut <amogh.raut@ril.com>
2022-08-08 12:15:56 +0530

    switch with fallthrough update

--- First.playground/Contents.swift
+++ First.playground/Contents.swift
@@ -80,4 +80,34 @@ else {
     print("Number is 0.")
 }
 
+//switch
+// program to find the day of the week
 
+let dayOfWeek = 4
+
+switch dayOfWeek {
+  case 1:
+    print("Sunday")
+        
+  case 2:
+    print("Monday")
+        
+  case 3:
+    print("Tuesday")
+        
+  case 4:
+    print("Wednesday")
+    fallthrough
+        
+  case 5:
+    print("Thursday")
+    fallthrough
+  case 6:
+    print("Friday")
+        
+  case 7:
+    print("Saturday")
+        
+  default:
+    print("Invalid day")
+}
