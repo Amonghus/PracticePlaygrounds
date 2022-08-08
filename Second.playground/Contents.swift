751e4a42311b545185fbe75858f877041a2e85e2
Amogh Raut <amogh.raut@ril.com>
2022-08-08 12:19:10 +0530

    updated inheritence

--- Second.playground/Contents.swift
+++ Second.playground/Contents.swift
@@ -14,8 +14,8 @@ func division(numerator: Float, denominator: Float) throws {
   }
     
   else {
-      //let result: Float
-      let result = Float(numerator / denominator)
+      let result: Float
+      result = Float(numerator / denominator)
     print(result)
   }
 }
@@ -30,3 +30,34 @@ do {
 catch DivisionError.dividedByZero {
   print("Error: Denominator cannot be 0")
 }
+//inheritence
+class Animal {
+
+  // properties and method of the parent class
+  var name: String = ""
+
+  func eat() {
+    print("I can eat")
+  }
+}
+
+// inherit from Animal
+class Dog: Animal {
+
+  // new method in subclass
+  func display() {
+
+    // access name property of superclass
+    print("My name is ", name);
+  }
+}
+
+// create an object of the subclass
+var labrador = Dog()
+
+// access superclass property and method
+labrador.name = "Moti"
+labrador.eat()
+
+// call subclass method
+labrador.display()
